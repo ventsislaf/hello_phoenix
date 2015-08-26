8a9dce17a29dd3c1038a56541c8fcea8e6180091
Ventsislav Nikolov <vencislaf@gmail.com>
2015-08-26 11:57:09 +0300

    Hello world page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule HelloPhoenix.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def hello(conn, _params) do
+    render conn, "hello.html"
+  end
 end
